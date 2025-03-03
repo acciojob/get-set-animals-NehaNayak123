eebf81b0f580eed4790b15e4526a553985e52cc0
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-03 21:34:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
+// //complete this code
+// class Animal {}
+
+// class Dog extends Animal {}
+
+// class Cat extends Animal {}
+
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
+// //////////////////////////////////
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species=species
+	}
+	get species() {
+        return this.species;
+    }
+	makeSound(){
+		console.log(`The ${this.species} makes a sound`)
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species)
+	}
+	bark(){
+		console.log("woof")
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+		super(species)
+	}
+	purr(){
+		console.log("purr")
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
+
+
+
+
