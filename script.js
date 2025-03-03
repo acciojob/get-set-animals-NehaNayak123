00e1413ca31a1b32d2bd55900a52469bb803fb6a
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-03 21:44:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,36 +11,76 @@
 // window.Cat = Cat;
 // //////////////////////////////////
 //complete this code
+// Base Animal class
 class Animal {
-	constructor(species){
-		this.species=species
-	}
-	getSpecies() {
-        return this.species;
+    constructor(species) {
+        this._species = species; // Store species in a private variable
     }
-	makeSound(){
-		// console.log(`The ${this.species} makes a sound`)
-		console.log(`The ${species()} makes a sound`)
-	}
+
+    // Getter for species
+    get species() {
+        return this._species;
+    }
+
+    // Method to log the animal sound message
+    makeSound() {
+        console.log(`The ${this._species} makes a sound`);
+    }
 }
 
-class Dog extends Animal {
-	constructor(species){
-		super(species)
-	}
-	bark(){
-		console.log("woof")
-	}
-}
-
+// Cat class inheriting from Animal
 class Cat extends Animal {
-	constructor(species){
-		super(species)
-	}
-	purr(){
-		console.log("purr")
-	}
+    constructor(species) {
+        super(species); // Call the Animal constructor
+    }
+
+    // Method to log "purr"
+    purr() {
+        console.log("purr");
+    }
 }
+
+// Dog class inheriting from Animal
+class Dog extends Animal {
+    constructor(species) {
+        super(species); // Call the Animal constructor
+    }
+
+    // Method to log "woof"
+    bark() {
+        console.log("woof");
+    }
+}
+// class Animal {
+// 	constructor(species){
+// 		this.species=species
+// 	}
+// 	getSpecies() {
+//         return this.species;
+//     }
+// 	makeSound(){
+// 		// console.log(`The ${this.species} makes a sound`)
+// 		console.log(`The ${species()} makes a sound`)
+// 	}
+// }
+
+// class Dog extends Animal {
+// 	constructor(species){
+// 		super(species)
+// 	}
+// 	bark(){
+// 		console.log("woof")
+// 	}
+// }
+
+// class Cat extends Animal {
+// 	constructor(species){
+// 		super(species)
+// 	}
+// 	purr(){
+// 		console.log("purr")
+// 	}
+// }
 
 // Do not change the code below this line
 window.Animal = Animal;
